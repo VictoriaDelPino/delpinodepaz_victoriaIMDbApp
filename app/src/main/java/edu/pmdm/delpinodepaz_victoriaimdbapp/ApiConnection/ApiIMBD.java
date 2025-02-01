cf0984c16106400ce7b602a8a9bc65b4e59f52b2
Victoria del Pino <vicky.geky@gmail.com>
2025-02-01 12:21:18 +0100

    Cargan las peliculas en el recyclerview

--- app/src/main/java/edu/pmdm/delpinodepaz_victoriaimdbapp/ApiConnection/ApiIMBD.java
+++ app/src/main/java/edu/pmdm/delpinodepaz_victoriaimdbapp/ApiConnection/ApiIMBD.java
@@ -1,136 +1,170 @@
 package edu.pmdm.delpinodepaz_victoriaimdbapp.ApiConnection;
 
 import android.util.Log;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.json.*;
-
 import edu.pmdm.delpinodepaz_victoriaimdbapp.Movies.Movie;
-
+import java.util.concurrent.CountDownLatch;
 
 public class ApiIMBD {
-    private static String headerKey= "x-rapidapi-key";
-    private static String headerHost= "x-rapidapi-host";
-    private static String apiKey= "586f49b130msh99155cbad4559f5p12735bjsn219e7f19e09e";
-    private static String headerValue="imdb-com.p.rapidapi.com";
-    private static Movie movie;
-    private static List<Movie> moviesList;
+    private static final String HEADER_KEY = "x-rapidapi-key";
+    private static final String HEADER_HOST = "x-rapidapi-host";
+    private static final String API_KEY = "5f871d3eeemsh5a94169685bb269p1e3fd8jsn031f2b0f5978";
+    private static final String HEADER_VALUE = "imdb-com.p.rapidapi.com";
+    private static final int TIMEOUT = 5000;
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(5);
+    private static final List<Movie> moviesList = new ArrayList<>();
 
-    public static void getTop10Movie(){
-        Thread thread=new Thread(()->{
-            try {
-                moviesList= new ArrayList<>();
-                String URLstring= ("https://imdb-com.p.rapidapi.com/title/get-top-meter?topMeterTitlesType=ALL");
-                URL url=new URL(URLstring);
-
-                HttpURLConnection conection= (HttpURLConnection) url.openConnection();
-                conection.setRequestMethod("GET");
-                conection.setRequestProperty(headerKey,apiKey);
-                conection.setRequestProperty(headerHost,headerValue );
-                conection.setConnectTimeout(5000);
-                conection.setReadTimeout(5000);
-
-                int responseCode=conection.getResponseCode();
-                BufferedReader bReader= new BufferedReader(new InputStreamReader(conection.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-                while((inputLine=bReader.readLine())!=null){
-                    response.append(inputLine);
-                }
-                bReader.close();
-                String json=response.toString();
-                JSONObject jsonObject= new JSONObject(json);
-                JSONObject jsonObjectData= jsonObject.getJSONObject("data");
-
-                JSONObject jsonObjectFilms= jsonObjectData.getJSONObject("topMeterTitles");
-                JSONArray filmsArray=jsonObjectFilms.getJSONArray("edges");
-                for (int i = 0; i < 10; i++) {
-                    movie=new Movie();
-                    JSONObject filmObject = filmsArray.getJSONObject(i); // Obtener cada objeto JSON dentro del array
-                    JSONObject movieObject=filmObject.getJSONObject("node");
-                    String movieID= movieObject.getString("id");
-                    Log.d("Victoria__id "+i,movieID);
-                    movie.setId(movieID);
-
-                    JSONObject titleTextObject = movieObject.getJSONObject("titleText");
-                    String titleText = titleTextObject.getString("text");
-                    Log.d("Victoria__title "+i,titleText);
-                    movie.setTitle(titleText);
-
-                    JSONObject releaseDateObject = movieObject.getJSONObject("releaseDate");
-                    int month = releaseDateObject.getInt("month");
-                    int day = releaseDateObject.getInt("day");
-                    int year = releaseDateObject.getInt("year");
-                    String releaseDate= day+"/"+month+"/"+year;
-                    Log.d("Victoria__releaseDate "+i,releaseDate);
-                    movie.setReleaseDate(releaseDate);
-
-                    JSONObject typeObject = movieObject.getJSONObject("titleType");
-                    String type = typeObject.getString("id");
-                    Log.d("Victoria__type "+i,type);
-
-                    JSONObject imgObject = movieObject.getJSONObject("primaryImage");
-                    String imgURL = imgObject.getString("url");
-                    Log.d("Victoria__url "+i,imgURL);
-                    movie.setPhoto(imgURL);
-
-                    JSONObject rankingObject = movieObject.getJSONObject("meterRanking");
-                    int ranking = rankingObject.getInt("currentRank");
-                    Log.d("Victoria__rank"+i,ranking+"");
-                    movie.setRating(ranking+"");
-
-                    Log.d("Victoria__","*****************************************************************");
-                    moviesList.add(movie);
-                    getMovieOverview(movieID);
-
-                }
-
-                Log.d("Victoria__","*****************************************************************");
-
-            }catch (Exception e){
-                Log.d("Victoria__2",e.getMessage());
-            }
-        });
-        thread.start();
+    public interface MovieOverviewCallback {
+        void onOverviewReceived(String overview);
     }
 
-    public static void getMovieOverview(String movieID){
-        Thread thread=new Thread(()->{
+    public static ArrayList<Movie> getTop10Movie() {
+        CountDownLatch countDownLatch= new CountDownLatch(1);
+        executorService.execute(() -> {
+
             try {
-                String URLstring= ("https://imdb-com.p.rapidapi.com/title/get-overview?tconst="+movieID);
-                URL url=new URL(URLstring);
+                String URLstring = "https://imdb-com.p.rapidapi.com/title/get-top-meter?topMeterTitlesType=ALL";
+                URL url = new URL(URLstring);
 
-                HttpURLConnection conection= (HttpURLConnection) url.openConnection();
-                conection.setRequestMethod("GET");
-                conection.setRequestProperty(headerKey,apiKey);
-                conection.setRequestProperty(headerHost,headerValue );
-                conection.setConnectTimeout(5000);
-                conection.setReadTimeout(5000);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty(HEADER_KEY, API_KEY);
+                connection.setRequestProperty(HEADER_HOST, HEADER_VALUE);
+                connection.setConnectTimeout(TIMEOUT);
+                connection.setReadTimeout(TIMEOUT);
 
-                int responseCode=conection.getResponseCode();
-                BufferedReader bReader= new BufferedReader(new InputStreamReader(conection.getInputStream()));
-                String inputLine;
+                if (connection.getResponseCode() != 200) {
+                    Log.d("Victoria__Error", "API Response Code: " + connection.getResponseCode());
+                    return;
+                }
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 StringBuilder response = new StringBuilder();
-                while((inputLine=bReader.readLine())!=null){
-                    response.append(inputLine);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
                 }
-                bReader.close();
-                if(response!=null) {
-                String json=response.toString();
-                JSONObject jsonObject= new JSONObject(json);//plot
-                Log.d("Victoria__Overview", jsonObject.toString());
+                reader.close();
+
+                JSONArray filmsArray = new JSONObject(response.toString())
+                        .getJSONObject("data")
+                        .getJSONObject("topMeterTitles")
+                        .getJSONArray("edges");
+
+                List<Movie> tempMoviesList = new ArrayList<>();
+                for (int i = 0; i < Math.min(filmsArray.length(), 10); i++) {
+                    JSONObject movieObject = filmsArray.getJSONObject(i).getJSONObject("node");
+                    Movie movie = new Movie();
+                    movie.setId(movieObject.getString("id"));
+                    movie.setTitle(movieObject.getJSONObject("titleText").getString("text"));
+                    movie.setPhoto(movieObject.getJSONObject("primaryImage").getString("url"));
+
+                    JSONObject releaseDateObject = movieObject.optJSONObject("releaseDate");
+                    if (releaseDateObject != null) {
+                        String releaseDate = releaseDateObject.optInt("day", 1) + "/" +
+                                releaseDateObject.optInt("month", 1) + "/" +
+                                releaseDateObject.optInt("year", 2000);
+                        movie.setReleaseDate(releaseDate);
+                    }
+
+                    movie.setRating(String.valueOf(movieObject.getJSONObject("meterRanking").optInt("currentRank", 0)));
+                    tempMoviesList.add(movie);
                 }
 
-            }catch (Exception e){
-                Log.d("Victoria__2",e.getMessage());
+                moviesList.clear();
+                moviesList.addAll(tempMoviesList);
+                CountDownLatch countDown= new CountDownLatch(moviesList.size());
+
+                for (int i = 0; i < moviesList.size(); i++) {
+                    Movie movie = moviesList.get(i);
+                    int finalI = i;
+                    getMovieOverview(movie.getId(), overview -> {
+                        movie.setDescription(overview);
+                        Log.d("Victoria__Overview", "Descripción recibida: " + overview);
+                        Log.d("Victoria__movie_" + finalI, movie.toString());
+                        countDown.countDown();
+                    });
+                }
+                try {
+                    countDown.await();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+            } catch (Exception e) {
+                Log.d("Victoria__Error", "Error: " + e.getMessage());
+            }
+
+            for (int i = 0; i < moviesList.size(); i++) {
+                Movie movie = moviesList.get(i);
+                Log.d("Victoria__movie___" + i, movie.toString());
+
+            }
+            countDownLatch.countDown();
+
+        });
+        try{
+            countDownLatch.await();
+        }catch (InterruptedException ei){
+            ei.printStackTrace();
+        }
+        return (ArrayList<Movie>) moviesList;
+    }
+
+    public static void getMovieOverview(String movieID, MovieOverviewCallback callback) {
+        executorService.submit(() -> {
+            try {
+                String URLstring = "https://imdb-com.p.rapidapi.com/title/get-overview?tconst=" + movieID;
+                URL url = new URL(URLstring);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty(HEADER_KEY, API_KEY);
+                connection.setRequestProperty(HEADER_HOST, HEADER_VALUE);
+                connection.setConnectTimeout(TIMEOUT);
+                connection.setReadTimeout(TIMEOUT);
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == 429) {
+                    Thread.sleep(3000);
+                    getMovieOverview(movieID, callback);
+                    return;
+                }
+                if (responseCode != 200) {
+                    Log.d("Victoria__Error", "API Response Code: " + responseCode);
+                    callback.onOverviewReceived("");
+                    return;
+                }
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                JSONObject jsonObject = new JSONObject(response.toString());
+                String plot = jsonObject.getJSONObject("data")
+                        .getJSONObject("title")
+                        .getJSONObject("plot")
+                        .getJSONObject("plotText")
+                        .getString("plainText");
+
+                callback.onOverviewReceived(plot);
+
+            } catch (Exception e) {
+                Log.d("Victoria__Error", "Error: " + e.getMessage());
+                callback.onOverviewReceived("");
             }
         });
-        thread.start();
     }
 }
--- app/src/main/java/edu/pmdm/delpinodepaz_victoriaimdbapp/ui/home/HomeFragment.java
+++ app/src/main/java/edu/pmdm/delpinodepaz_victoriaimdbapp/ui/home/HomeFragment.java
@@ -1,20 +1,31 @@
 package edu.pmdm.delpinodepaz_victoriaimdbapp.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.pmdm.delpinodepaz_victoriaimdbapp.ApiConnection.ApiIMBD;
+import edu.pmdm.delpinodepaz_victoriaimdbapp.Movies.Movie;
+import edu.pmdm.delpinodepaz_victoriaimdbapp.MyItemRecycleViewAdapter;
 import edu.pmdm.delpinodepaz_victoriaimdbapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private MyItemRecycleViewAdapter adapter;
+    private List<Movie> movieList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +35,37 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
+        // Configurar RecyclerView
+        setupRecyclerView();
 
         return root;
+    }
+
+    private void setupRecyclerView() {
+        // Crear lista de películas de prueba
+        movieList = ApiIMBD.getTop10Movie();
+        Log.d("Victoria__recyclerview", String.valueOf(movieList.size()));
+      /*  movieList.add(new Movie("Inception", "1", "https://image-url.com/inception.jpg","Desc","Sci-Fi","8.8"));
+        movieList.add(new Movie("Interstellar", "2", "https://image-url.com/interstellar.jpg","Desc","Sci-Fi","8.6"));
+        movieList.add(new Movie("The Dark Knight", "3", "https://image-url.com/darkknight.jpg","Desc","Action","9.0"));
+        movieList.add(new Movie("Tenet", "4", "https://image-url.com/tenet.jpg","Desc","Thriller","7.5"));
+*/
+        // Configurar RecyclerView con GridLayoutManager para 2 columnas
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        adapter = new MyItemRecycleViewAdapter(movieList, getContext(), new MyItemRecycleViewAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Movie movie) {
+                Toast.makeText(getContext(), "Clic en: " + movie.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(Movie movie) {
+                Toast.makeText(getContext(), "Manteniendo: " + movie.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -35,3 +74,4 @@ public class HomeFragment extends Fragment {
         binding = null;
     }
 }
+
